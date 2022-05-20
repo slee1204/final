23878722c2e66a3e45f7961046401dfe00cf5c3a
SueLee <slee1008@my.bcit.ca>
2022-05-20 04:04:19 -0700

    revert

--- next.config.js
+++ next.config.js
@@ -7,10 +7,7 @@ const nextConfig = {
 }
 
 module.exports = {
-  images: {
-    loader: 'imgix',
-    domain: [process.env.CLIENT],
-  },
+  
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
